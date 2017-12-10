b4e79577acd720290b609fb23f54adbba29f0510
阅卷 <32693484+littleguyy@users.noreply.github.com>
2017-12-10 13:39:17 +0800

    http代理改动
    http代理接受一条数据包后，删掉了连接，导致客户端口第二个数据包，服务器接收不到。
    删掉连接，客户端口变了。客户端收到代理可用后，发送的数据收不到了。
    这次在删之前，加了个接收客户端数据，就可以接收到服务器确定信号后的客户端数据了。
    ###总也接收不到客户端正式网页get数据，我还以为是socket有什么没配置呢。还用wireshark监视了半天，能看到客户端发送的get数据，就是python服务器程序收不到。原来是服务器收了一个数据包，就把接受连接关闭了。

--- http代理原理.py
+++ http代理原理.py
@@ -1,20 +1,31 @@
-if __name__ == '__main__':  
-    import socket
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  
-    sock.bind(("0.0.0.0", 1080))  
-    sock.listen(1)  
+import socket
+import os
+import sys
+
+if __name__ == '__main__':
+    ServerHost='127.0.0.1'
+    ServerPort=1081
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    print("服务器的socket建立了")
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind((ServerHost, ServerPort))
+    print('服务器的socket绑定到%s:%d'%(ServerHost,ServerPort))
+    sock.listen(500)
+    print('服务器开始监听端口%d，等待连接。'%ServerPort)    
     while True:
-        print("OK.....................")
         connection,address = sock.accept()
-        print("OK..........ok...........")
-        try:  
-            # connection.settimeout(5)  
-            buf = connection.recv(10240)
-            if buf:  
-                print('%s'%buf)
-                connection.send(b'HTTP/1.1 200 Connection Established\r\n\r\n')
-            else:  
-                print("error") 
+        print("服务器sock连接到客户端地址：",address)
+        try:
+            connection.settimeout(5)
+            buf = connection.recv(2048)
+            print('客户端发来数据：%s'%buf)
+            # if headers["Method"]=="CONNECT":
+            connection.send(b'HTTP/1.1 200 Connection Established\r\n\r\n')
+            print("向客户端发送了HTTP/1.1 200 Connection Established\r\n\r\n")
+            buf=connection.recv(2048)
+            print('客户端再次发来数据：%s'%buf)
+            #else:  
+                #print("error") 
         except socket.timeout:  
             print ('time out')
         connection.close()
